89dd89b94f37ab3c4157ffc82eded184bd35ed8d
Valeriy <wiep1994#yandex.ru>
2024-11-03 20:54:14 +0300

    new

---
+++ HomeWork/Transactions/src/main/java/org/example/Account.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Account {
+
+    private long money;
+    private String accNumber;
+
+    public long getMoney() {
+        return money;
+    }
+
+    public void setMoney(long money) {
+        this.money = money;
+    }
+
+    public String getAccNumber() {
+        return accNumber;
+    }
+
+    public void setAccNumber(String accNumber) {
+        this.accNumber = accNumber;
+    }
+}
